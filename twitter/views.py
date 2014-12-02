f5b44b1a6f8d341e5751d45741eed01919fc0e9b
luisriq <luis.9993@gmail.com>
2014-12-01 22:02:29 -0300

    Modificando tablas

--- twitter/views.py
+++ twitter/views.py
@@ -5,8 +5,9 @@ from .models import Tweet
 def home(request):
 	return HttpResponse("Hola mundo!!!!")
 def home(request, otro):
-	res = "<!DOCTYPE html>"+"<html>"+"<body>"
-	res += "<table border=\"2\" style=\"width:100%\">"
+	res = "<!DOCTYPE html>"+"<html>"
+	res +="<head><<STYLE type= \"text/css\"> td {text-align: center;padding:0px 25px 0px 25px} </STYLE> </head> <body>"
+	res += "<table border=\"2\" >"
 	res += "<tr><td>msg</td><td>retweets</td><td>favs</td>"
 	for t in Tweet.objects.all():
 		res += "<tr><td>"+t.msg+"</td>"+"<td>%d</td>" % t.favs+"<td>%d</td>" % (t.retweets)+"</tr>"
