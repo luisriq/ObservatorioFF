68f71d8fc6d7abb7426b25050d459b05b311fd16
luisriq <luis.9993@gmail.com>
2014-12-01 21:27:12 -0300

    Agregando lectura /casa

--- cadenas/urls.py
+++ cadenas/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', include('twitter.urls'), name='home'),
-    url(r'^casa/(\C*)$', 'twitter.views.home', name='home'),
+    url(r'^casa/(\w*)$', 'twitter.views.home', name='home'),
 )
--- twitter/views.py
+++ twitter/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Tweet
 def home(request):
 	return HttpResponse("Hola mundo!!!!")
 def home(request, otro):
+	res = ""
+	res += "|msg\t|retweets\t|favs"
+	for t in Tweet.objects.all():
+		res += "<br />"+t.msg+"\t"+"%d" % t.favs+"\t%d" % (t.retweets) 
+	
 	if(str(otro).isdigit()):
-		return HttpResponse("Hola %d mundo!!!!" % int(otro))
+		return HttpResponse(res+"<br />Hola %d mundo!!!!" % int(otro))
 	else:
-		return HttpResponse("Hola mundo!!!! no muneros"+str(otro))
+		return HttpResponse(res+"<br />Hola mundo!!!! no muneros"+str(otro)+str(tweet.msg))
