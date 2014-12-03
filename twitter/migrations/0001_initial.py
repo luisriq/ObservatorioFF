c96e6f2a3b0aaceef550c113d01d38f477166f36
Cristian Gomez <cristian.gomezi@usach.cl>
2014-12-03 12:52:05 -0300

    configurando

--- twitter/migrations/0001_initial.py
+++ twitter/migrations/0001_initial.py
@@ -11,14 +11,186 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Cadena',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(default=b'', max_length=255)),
+            ],
+            options={
+                'verbose_name_plural': 'Cadena',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Ciudad',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(default=b'', max_length=255)),
+            ],
+            options={
+                'verbose_name_plural': 'Ciudad',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Comida',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(default=b'', max_length=255)),
+            ],
+            options={
+                'verbose_name_plural': 'Comida',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Contiene',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+                'verbose_name_plural': 'Contiene',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Evento',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('fecha', models.DateTimeField(auto_now=True)),
+                ('nombre', models.CharField(default=b'', max_length=255)),
+                ('id_ciudad', models.ForeignKey(to='twitter.Cadena')),
+            ],
+            options={
+                'verbose_name_plural': 'Evento',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Hashtag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(default=b'', max_length=255)),
+                ('id_cadena', models.ForeignKey(to='twitter.Cadena')),
+            ],
+            options={
+                'verbose_name_plural': 'Hashtag',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Menciona',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+                'verbose_name_plural': 'Menciona',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Pais',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(default=b'', max_length=255)),
+            ],
+            options={
+                'verbose_name_plural': 'Pais',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Prepara',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id_comida', models.ForeignKey(to='twitter.Comida')),
+            ],
+            options={
+                'verbose_name_plural': 'Prepara',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Referencia',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id_comida', models.ForeignKey(to='twitter.Comida')),
+            ],
+            options={
+                'verbose_name_plural': 'Referencia',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Representa',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id_cadena', models.ForeignKey(to='twitter.Cadena')),
+            ],
+            options={
+                'verbose_name_plural': 'Representa',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Sigue',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id_cadena', models.ForeignKey(to='twitter.Cadena')),
+            ],
+            options={
+                'verbose_name_plural': 'Sigue',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Tweet',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('retweets', models.PositiveIntegerField()),
                 ('favs', models.PositiveIntegerField()),
+                ('msg', models.CharField(default=b'', max_length=255)),
             ],
             options={
+                'verbose_name_plural': 'Tweet',
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Usuario',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('seguidores', models.PositiveIntegerField()),
+                ('cuenta', models.CharField(default=b'', max_length=255)),
+                ('id_ciudad', models.ForeignKey(to='twitter.Ciudad')),
+            ],
+            options={
+                'verbose_name_plural': 'Usuario',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='tweet',
+            name='id_usuario',
+            field=models.ForeignKey(to='twitter.Usuario'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='menciona',
+            name='id_usuario',
+            field=models.ForeignKey(to='twitter.Usuario'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contiene',
+            name='id_hashtag',
+            field=models.ForeignKey(to='twitter.Hashtag'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='ciudad',
+            name='id_pais',
+            field=models.ForeignKey(to='twitter.Pais'),
+            preserve_default=True,
         ),
     ]
--- twitter/models.py
+++ twitter/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Pais(models.Model):
 	nombre = models.CharField(max_length = 255, default = '')
-		class Meta:
-			verbose_name_plural = "Pais"
+	class Meta:
+		verbose_name_plural = "Pais"
 	def __unicode__(self):
 		return self.nombre
 
@@ -12,7 +12,7 @@ class Ciudad(models.Model):
 	nombre = models.CharField(max_length = 255, default = '')
 	id_pais = models.ForeignKey(Pais)
 	class Meta:
-			verbose_name_plural = "Ciudad"
+		verbose_name_plural = "Ciudad"
 	def __unicode__(self):
 		return self.nombre
 
@@ -21,7 +21,7 @@ class Usuario(models.Model):
 	cuenta = models.CharField(max_length = 255, default = '')
 	id_ciudad = models.ForeignKey(Ciudad)
 	class Meta:
-			verbose_name_plural = "Usuario"
+		verbose_name_plural = "Usuario"
 	def __unicode__(self):
 		return self.cuenta
 
@@ -31,14 +31,14 @@ class Tweet(models.Model):
 	msg = models.CharField(max_length=255, default='')
 	id_usuario = models.ForeignKey(Usuario)
 	class Meta:
-			verbose_name_plural = "Tweet"
+		verbose_name_plural = "Tweet"
 	def __unicode__(self):
 		return self.msg
 
 class Cadena(models.Model):
 	nombre = models.CharField(max_length = 255, default = '')
 	class Meta:
-			verbose_name_plural = "Cadena"
+		verbose_name_plural = "Cadena"
 	def __unicode__(self):
 		return self.nombre
 
@@ -46,14 +46,14 @@ class Hashtag(models.Model):
 	nombre = models.CharField(max_length = 255, default = '')
 	id_cadena = models.ForeignKey(Cadena)
 	class Meta:
-			verbose_name_plural = "Hashtag"
+		verbose_name_plural = "Hashtag"
 	def __unicode__(self):
 		return self.nombre
 
 class Comida(models.Model):
 	nombre = models.CharField(max_length = 255, default = '')
 	class Meta:
-			verbose_name_plural = "Comida"
+		verbose_name_plural = "Comida"
 	def __unicode__(self):
 		return self.nombre
 
@@ -63,42 +63,42 @@ class Evento(models.Model):
 	nombre = models.CharField(max_length = 255, default = '')
 	id_ciudad = models.ForeignKey(Cadena)
 	class Meta:
-			verbose_name_plural = "Evento"
+		verbose_name_plural = "Evento"
 	def __unicode__(self):
 		return self.nombre
 
 class Contiene(models.Model):
 	id_hashtag = models.ForeignKey(Hashtag)
 	class Meta:
-			verbose_name_plural = "Contiene"
+		verbose_name_plural = "Contiene"
 	def __unicode__(self):
 		return self.nombre
 
 class Menciona(models.Model):
 	id_usuario = models.ForeignKey(Usuario)
 	class Meta:
-			verbose_name_plural = "Menciona"
+		verbose_name_plural = "Menciona"
 	def __unicode__(self):
 		return self.nombre
 
 class Referencia(models.Model):
 	id_comida = models.ForeignKey(Comida)
 	class Meta:
-			verbose_name_plural = "Referencia"
+		verbose_name_plural = "Referencia"
 	def __unicode__(self):
 		return self.nombre
 
 class Sigue(models.Model):
 	id_cadena = models.ForeignKey(Cadena)
 	class Meta:
-			verbose_name_plural = "Sigue"
+		verbose_name_plural = "Sigue"
 	def __unicode__(self):
 		return self.nombre
 
 class Representa(models.Model):
 	id_cadena = models.ForeignKey(Cadena)
 	class Meta:
-			verbose_name_plural = "Representa"
+		verbose_name_plural = "Representa"
 	def __unicode__(self):
 		return self.nombre
 
@@ -106,6 +106,6 @@ class Representa(models.Model):
 class Prepara(models.Model):
 	id_comida = models.ForeignKey(Comida)
 	class Meta:
-			verbose_name_plural = "Prepara"
+		verbose_name_plural = "Prepara"
 	def __unicode__(self):
 		return self.nombre
