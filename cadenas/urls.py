6e0f4aa299fc6391c64d77657964f3beeb706cbf
firenessmax <fireness@Fireness.iptv\.microsoft\.com>
2014-12-06 21:56:57 -0300

    archivos estaticos
    se agrega soporte para archivos estaticos… afin

--- cadenas/urls.py
+++ cadenas/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf.urls.static import static
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cadenas.views.home', name='home'),
@@ -8,4 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('twitter.urls'), name='home'),
+    
 )
