1383bccf975325cf468ce939b742e806e1efb567
Cristian Gomez <cristian.gomezi@usach.cl>
2014-12-12 17:12:48 -0300

    Reparando modelos

--- twitter/models.py
+++ twitter/models.py
@@ -8,6 +8,13 @@ class Pais(models.Model):
 	def __unicode__(self):
 		return self.nombre
 
+	def crearPais(nombre):
+		try:
+			pais = pais.objects.get(nombre)
+		except:
+			pais = pais()
+			pais.nombre = nombre
+
 class Ciudad(models.Model):
 	nombre = models.CharField(max_length = 255, default = '')
 	id_pais = models.ForeignKey(Pais)
@@ -15,6 +22,16 @@ class Ciudad(models.Model):
 		verbose_name_plural = "Ciudad"
 	def __unicode__(self):
 		return self.nombre
+	
+	def crearCiudad(nombre, pais):
+		try:
+			ciudad = Ciudad.objects.get(nombre)
+		except:
+			ciudad = Ciudad()
+			ciudad.nombre = nombre
+			ciudad.id_pais = pais
+
+
 
 class Usuario(models.Model):
 	seguidores = models.PositiveIntegerField()
@@ -24,6 +41,17 @@ class Usuario(models.Model):
 		verbose_name_plural = "Usuario"
 	def __unicode__(self):
 		return self.cuenta
+
+	def crearUsuario(nombre, ciudad):
+		try:
+			usuario = Usuario.objects.get(nombre)
+			return usuario
+		except:
+			usuario = Usuario()
+			usuario.nombre = nombre
+			usuario.id_ciudad = ciudad
+			return usuario
+
 
 class Tweet(models.Model):
 	retweets = models.PositiveIntegerField()
@@ -69,6 +97,7 @@ class Evento(models.Model):
 
 class Contiene(models.Model):
 	id_hashtag = models.ForeignKey(Hashtag)
+	id_tweet = models.ForeignKey(Tweet)
 	class Meta:
 		verbose_name_plural = "Contiene"
 	def __unicode__(self):
@@ -76,6 +105,7 @@ class Contiene(models.Model):
 
 class Menciona(models.Model):
 	id_usuario = models.ForeignKey(Usuario)
+	id_tweet = models.ForeignKey(Tweet)
 	class Meta:
 		verbose_name_plural = "Menciona"
 	def __unicode__(self):
@@ -83,6 +113,7 @@ class Menciona(models.Model):
 
 class Referencia(models.Model):
 	id_comida = models.ForeignKey(Comida)
+	id_hashtag = models.ForeignKey(Hashtag)
 	class Meta:
 		verbose_name_plural = "Referencia"
 	def __unicode__(self):
@@ -90,6 +121,7 @@ class Referencia(models.Model):
 
 class Sigue(models.Model):
 	id_cadena = models.ForeignKey(Cadena)
+	id_usuario = models.ForeignKey(Usuario)
 	class Meta:
 		verbose_name_plural = "Sigue"
 	def __unicode__(self):
@@ -97,6 +129,7 @@ class Sigue(models.Model):
 
 class Representa(models.Model):
 	id_cadena = models.ForeignKey(Cadena)
+	id_usuario = models.ForeignKey(Usuario)
 	class Meta:
 		verbose_name_plural = "Representa"
 	def __unicode__(self):
@@ -105,6 +138,7 @@ class Representa(models.Model):
 
 class Prepara(models.Model):
 	id_comida = models.ForeignKey(Comida)
+	id_cadena = models.ForeignKey(Cadena)
 	class Meta:
 		verbose_name_plural = "Prepara"
 	def __unicode__(self):
